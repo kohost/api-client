c2f09e60080926536cde633c3c94ecdc7383b409
Ian Rogers <ian@kohost.io>
2020-12-09 17:01:02 -0800

    save user data to LS on get and put routes for user

--- src/methods/User.js
+++ src/methods/User.js
@@ -31,6 +31,39 @@ function resetPassword(userID, { password, resetToken }) {
   });
 }
 
+function saveUserData(id, response) {
+  // save the new data back to LS if the user is updating itself
+  if (id === this.getCurrentUser()._id) {
+    const { data, headers } = response;
+    // save the current user object
+    if (data) {
+      const user = data && data[0];
+      this.setCurrentUser(user);
+    }
+
+    if (headers) {
+      // if the user updates itself, the API will send back a new token, lets save it
+      const authToken = headers[this.authTokenKey];
+      const refreshToken = headers[this.refreshTokenKey];
+      if (authToken) this.setAuthToken(authToken);
+      if (refreshToken) this.setRefreshToken(refreshToken);
+    }
+  }
+  return response;
+}
+
+Users.update = function (id, body) {
+  const url = `${base}/${id}`;
+  const saveUser = saveUserData.bind(this);
+  return this.put(url, body).then((response) => saveUser(id, response));
+};
+
+Users.get = function (id) {
+  const url = `${base}/${id}`;
+  const saveUser = saveUserData.bind(this);
+  return this.get(url).then((response) => saveUser(id, response));
+};
+
 Users.changeAvatar = changeAvatar;
 Users.checkout = checkout;
 Users.resetPassword = resetPassword;
