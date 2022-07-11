5383119f155d00b6998b677d15acd6f2a401e2f4
Ian Rogers <ian@kohost.io>
2022-07-11 16:11:46 -0700

    remove mongoose

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const Models = require("./models");
-const { toMongoose } = require("./utils/compiler");
 // export all classes on the Models object
 module.exports = {
   Models,
-  toMongoose,
 };
