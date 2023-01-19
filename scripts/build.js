30e348ad89c0e487f4493268025fba14b77354bc
Ian Rogers <ian@kohost.io>
2023-01-19 14:00:24 -0800

    add watch option

--- scripts/build.js
+++ scripts/build.js
@@ -43,15 +43,15 @@ let useCasePlugin = {
         
             /* 
               Creates a method for each use case in the API
-              @param {Object} options - The options to send to the API
-              @param {Object} options.headers - The headers to send to the API
-              @param {Object} options.data - The body to send to the API. Valid for POST and PUT requests
-              @parms {Object} options.query - The query for the request to build the URL
+              @param {Object} requestData - The options to send to the API
+              @param {Object} requestData.headers - The headers to send to the API
+              @param {Object} requestData.data - The body to send to the API. Valid for POST and PUT requests
+              @parms {Object} requestData.query - The query for the request to build the URL
               @returns {Promise} The response from the API
             */
             
             //eslint-disable-next-line no-inner-declarations
-            module.exports = function ${useCase}(requestData, options = {}) {
+            module.exports = function ${useCase}(requestData = {data: null, query: null, headers: null}, httpConfigOptions = {}) {
 
             if (!requestData) requestData = {};
             
@@ -82,11 +82,11 @@ let useCasePlugin = {
             const config = {
               method: "${method.toLowerCase()}",
               url: url,
-              ...options,
+              ...httpConfigOptions,
             };
             
             if (data) config.data = data;
-            if (query) config.params = query;
+            if (query) config.query = query;
             if (headers) config.headers = headers;
             
             return this._http.request(config);
@@ -145,100 +145,139 @@ const entryPoints = [
   { in: path.resolve(__dirname, "../src/defs/"), out: "defs" },
 ];
 
-esbuild.build({
-  entryPoints: entryPoints,
-  bundle: true,
-  platform: "node",
-  packages: "external",
-  target: ["node16"],
-  outdir: "dist/cjs",
-  treeShaking: true,
-  allowOverwrite: true,
-});
+async function main() {
+  const build1 = await esbuild.context({
+    entryPoints: entryPoints,
+    bundle: true,
+    platform: "node",
+    packages: "external",
+    target: ["node16"],
+    outdir: "dist/cjs",
+    treeShaking: true,
+    allowOverwrite: true,
+  });
 
-esbuild.build({
-  entryPoints: entryPoints,
-  bundle: true,
-  sourcemap: true,
-  minify: false,
-  format: "esm",
-  target: "esnext",
-  outdir: "dist/esm",
-  define: { global: "window" },
-});
+  const build2 = await esbuild.context({
+    entryPoints: entryPoints,
+    bundle: true,
+    sourcemap: true,
+    minify: false,
+    format: "esm",
+    target: "esnext",
+    outdir: "dist/esm",
+    define: { global: "window" },
+  });
 
-esbuild.build({
-  plugins: [useCasePlugin],
-  entryPoints: [path.resolve(__dirname, "../src/Client/")],
-  bundle: true,
-  sourcemap: true,
-  minify: false,
-  format: "esm",
-  target: "esnext",
-  outdir: "dist/esm",
-  define: { global: "window" },
-});
+  const build3 = await esbuild.context({
+    plugins: [useCasePlugin],
+    entryPoints: [path.resolve(__dirname, "../src/Client/")],
+    bundle: true,
+    sourcemap: true,
+    minify: false,
+    format: "esm",
+    target: "esnext",
+    outdir: "dist/esm",
+    define: { global: "window" },
+  });
 
-esbuild.build({
-  plugins: [useCasePlugin],
-  entryPoints: [
-    { in: path.resolve(__dirname, "../src/Client/"), out: "Client" },
-  ],
-  bundle: true,
-  platform: "node",
-  target: ["node16"],
-  format: "cjs",
-  outfile: "dist/cjs/Client.js",
-  packages: "external",
-  allowOverwrite: true,
-});
+  const build4 = await esbuild.context({
+    plugins: [useCasePlugin],
+    entryPoints: [
+      { in: path.resolve(__dirname, "../src/Client/"), out: "Client" },
+    ],
+    bundle: true,
+    platform: "node",
+    target: ["node16"],
+    format: "cjs",
+    outfile: "dist/cjs/Client.js",
+    packages: "external",
+    allowOverwrite: true,
+  });
 
-esbuild.build({
-  plugins: [useCasePlugin],
-  entryPoints: [
-    { in: path.resolve(__dirname, "../src/Client/"), out: "Client" },
-  ],
-  bundle: true,
-  sourcemap: true,
-  minify: false,
-  format: "esm",
-  target: "esnext",
-  outdir: "dist/esm",
-  define: { global: "window" },
-  allowOverwrite: true,
-});
+  const build5 = await esbuild.context({
+    plugins: [useCasePlugin],
+    entryPoints: [
+      { in: path.resolve(__dirname, "../src/Client/"), out: "Client" },
+    ],
+    bundle: true,
+    sourcemap: true,
+    minify: false,
+    format: "esm",
+    target: "esnext",
+    outdir: "dist/esm",
+    define: { global: "window" },
+    allowOverwrite: true,
+  });
 
-esbuild.build({
-  entryPoints: [path.resolve(__dirname, "../src/index.js")],
-  bundle: false,
-  platform: "node",
-  packages: "external",
-  target: ["node16"],
-  outfile: "dist/cjs/index.cjs.js",
-  format: "cjs",
-});
+  const build6 = await esbuild.context({
+    entryPoints: [path.resolve(__dirname, "../src/index.js")],
+    bundle: false,
+    platform: "node",
+    packages: "external",
+    target: ["node16"],
+    outfile: "dist/cjs/index.cjs.js",
+    format: "cjs",
+  });
 
-esbuild.build({
-  entryPoints: [path.resolve(__dirname, "../src/index.js")],
-  bundle: false,
-  platform: "node",
-  packages: "external",
-  target: ["node16"],
-  outfile: "dist/cjs/index.cjs.js",
-  format: "cjs",
-});
+  const build7 = await esbuild.context({
+    entryPoints: [path.resolve(__dirname, "../src/index.js")],
+    bundle: false,
+    platform: "node",
+    packages: "external",
+    target: ["node16"],
+    outfile: "dist/cjs/index.cjs.js",
+    format: "cjs",
+  });
 
-fs.writeFileSync(
-  path.resolve(__dirname, "../dist/esm/index.js"),
-  `import Client from "./Client";
-  import Commands from "./Commands";
-  import Events from "./Events";
-  import Models from "./Models";
-  import Errors from "./Errors";
-  import defs from "./defs";
-  import utils from "./utils";
-  
-  export
-   { Client, Commands, Events, Models, Errors, defs, utils };
-  `
-);
+  fs.writeFileSync(
+    path.resolve(__dirname, "../dist/esm/index.js"),
+    `import Client from "./Client";
+    import Commands from "./Commands";
+    import Events from "./Events";
+    import Models from "./Models";
+    import Errors from "./Errors";
+    import defs from "./defs";
+    import utils from "./utils";
+    
+    export
+     { Client, Commands, Events, Models, Errors, defs, utils };
+    `
+  );
+  const watch = process.argv.includes("--watch");
+  if (watch) {
+    console.log("Watching for changes...");
+    const watcher = async () => {
+      await build1.watch();
+      await build2.watch();
+      await build3.watch();
+      await build4.watch();
+      await build5.watch();
+      await build6.watch();
+      await build7.watch();
+    };
+
+    watcher();
+  } else {
+    // run the build
+    console.log("building...");
+    await build1.rebuild();
+    await build2.rebuild();
+    await build3.rebuild();
+    await build4.rebuild();
+    await build5.rebuild();
+    await build6.rebuild();
+    await build7.rebuild();
+
+    build1.dispose();
+    build2.dispose();
+    build3.dispose();
+    build4.dispose();
+    build5.dispose();
+    build6.dispose();
+    build7.dispose();
+
+    console.log("done");
+  }
+}
+
+main();
