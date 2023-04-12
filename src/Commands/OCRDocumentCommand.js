2edf01b301a96c70d76cfcfbf269d9e83943f196
Ian Rogers <ian@itrogers.com>
2023-04-12 16:39:11 -0700

    remove requirement for type in OCR document

--- src/Commands/OCRDocumentCommand.js
+++ src/Commands/OCRDocumentCommand.js
@@ -3,7 +3,6 @@ const RequestError = require("../Errors/RequestError");
 
 class OCRDocumentCommand extends Command {
   constructor({ type, image }) {
-    if (!type) throw new RequestError("document type is required");
     if (!image) throw new RequestError("document image is required");
     super({ type, image });
   }
