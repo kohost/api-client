59dbc151d6637861a6d7993794448e926990a1a0
Ian Rogers <ian@itrogers.com>
2023-09-27 06:07:47 -0700

    ticket modeling

--- src/Commands/CheckOutReservationCommand.js
+++ src/Commands/CheckOutReservationCommand.js
@@ -2,9 +2,10 @@ const Command = require("./Command");
 const RequestError = require("../Errors/RequestError");
 
 class CheckOutReservationCommand extends Command {
-  constructor({ id, ...rest }) {
-    if (!id) throw new RequestError("reservation id is required");
-    super({ id, ...rest });
+  constructor({ reservationId, userId, ...rest }) {
+    if (!reservationId) throw new RequestError("reservation id is required");
+    if (!userId) throw new RequestError("user id is required");
+    super({ reservationId, userId, ...rest });
   }
 
   get name() {
