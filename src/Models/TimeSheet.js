5561459edcf4e68e4c3e8c190e872e16092cc695
Ian Rogers <ian@itrogers.com>
2023-11-21 17:56:51 -0800

    checkpoint

--- src/Models/TimeSheet.js
+++ src/Models/TimeSheet.js
@@ -3,6 +3,8 @@ const schemas = require("../utils/schema");
 const schema = require("../schemas/timeSheet.json");
 const Entity = require("./Entity");
 
+const { nanoid } = require("nanoid");
+
 schemas.add(schema);
 const validator = schemas.compile(schema);
 
@@ -14,7 +16,30 @@ class TimeSheet extends Entity {
    * @param {TimeSheetType} timesheet - The timesheet object of type TimeSheet.
    */
   constructor(timesheet) {
+    if (timesheet.day) {
+      timesheet.day = new Date(timesheet.day);
+    }
+
+    if (timesheet.timeEntries && Array.isArray(timesheet.timeEntries)) {
+      timesheet.timeEntries = timesheet.timeEntries.map((t) => {
+        if (!t.id) t.id = TimeSheet.generateTimeEntryId();
+        if (t.start) t.start = new Date(t.start);
+        if (t.end) t.end = new Date(t.end);
+        return t;
+      });
+
+      // sort by start time
+      timesheet.timeEntries.sort((a, b) => {
+        if (a.start < b.start) return -1;
+        if (a.start > b.start) return 1;
+        return 0;
+      });
+    }
     super(timesheet);
+  }
+
+  static generateTimeEntryId(len = 16) {
+    return nanoid(len);
   }
 }
 
