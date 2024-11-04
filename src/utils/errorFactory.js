69d6adc101459302506a5f11b042c3e4d3b8f5af
Ian Rogers <ian@itrogers.com>
2024-11-04 07:41:30 -0800

    concierge settings

--- src/utils/errorFactory.js
+++ src/utils/errorFactory.js
@@ -1,8 +1,32 @@
 const Errors = require("../Errors");
 
 module.exports = function errorFactory(errName) {
-  const AllErrors = Object.values(Errors);
-  const TheError = AllErrors.find((E) => E.prototype.name === errName);
-  if (!TheError) return new Error("Invalid error name: " + errName);
-  return TheError;
+  switch (errName) {
+    case "AppError":
+      return Errors.AppError;
+    case "AuthenticationError":
+      return Errors.AuthenticationError;
+    case "AuthorizationError":
+      return Errors.AuthorizationError;
+    case "ConflictError":
+      return Errors.ConflictError;
+    case "DeviceCommError":
+      return Errors.DeviceCommError;
+    case "LoginError":
+      return Errors.LoginError;
+    case "NotFoundError":
+      return Errors.NotFoundError;
+    case "RequestError":
+      return Errors.RequestError;
+    case "SystemCommError":
+      return Errors.SystemCommError;
+    case "TokenExpiredError":
+      return Errors.TokenExpiredError;
+    case "UnprocessableRequestError":
+      return Errors.UnprocessableRequestError;
+    case "ValidationError":
+      return Errors.ValidationError;
+    default:
+      return new Error("Invalid error name: " + errName);
+  }
 };
