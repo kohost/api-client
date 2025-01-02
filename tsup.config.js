a57c551ac952fa2941943e001a0ef9bca93ece83
Ian Rogers <ian@itrogers.com>
2025-01-02 13:01:25 -0800

    fix

---
+++ tsup.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.js"],
+  outDir: "dist",
+  format: ["cjs", "esm"],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  target: ["node20", "browser"],
+});
