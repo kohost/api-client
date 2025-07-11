d62d84c5bae08c1a5fafc7d5cfa770ae872d312f
Ian Rogers <ian@itrogers.com>
2025-07-11 07:08:13 -0700

    add session scheme

---
+++ src/schemas/session.js
@@ -0,0 +1,33 @@
+export default {
+  $schema: "http://json-schema.org/draft-07/schema",
+  $id: "session.json",
+  title: "Session",
+  type: "object",
+  required: ["id", "type", "title", "destination", "url", "systemId"],
+  properties: {
+    id: {
+      $ref: "definitions.json#/definitions/id",
+    },
+    type: {
+      type: "string",
+      default: "session",
+      enum: ["session"],
+    },
+    userId: {
+      type: "string",
+    },
+    organizationId: {
+      type: "string",
+    },
+    userAgent: {
+      type: "string",
+    },
+    expires: {
+      type: ["string", "object", "null"],
+    },
+    data: {
+      type: "object",
+      additionalProperties: true,
+    },
+  },
+};
