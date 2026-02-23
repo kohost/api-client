5e26b85aeb493f15ddaf3a49979ab9097f8bd8fa
Ian Rogers <ian@itrogers.com>
2026-02-23 12:20:55 -0800

    mobile mule live stream

--- src/schemas/camera.ts
+++ src/schemas/camera.ts
@@ -53,6 +53,7 @@ export const cameraSchema = {
             "digital-watchdog",
             "verkada",
             "mediamtx",
+            "mobile-mule",
           ],
         },
         allowedOrigins: {
